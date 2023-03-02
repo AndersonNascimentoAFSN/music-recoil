81841491b90bc6b6a5323fd7c75244e593863b52
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-03-02 10:53:42 -0300

    refatoração: configuração inicial styled-component

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
 
 import { DebugObserver } from "@/store/utils/DebugObserver";
 import { initRecoilState } from "@/store/utils/initRecoilState";
 
+import { defaultTheme } from "@/styles/theme/default";
+
 import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  <RecoilRoot initializeState={initRecoilState}>
-    <DebugObserver />
-    <App />
-  </RecoilRoot>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <RecoilRoot initializeState={initRecoilState}>
+      <DebugObserver />
+      <ThemeProvider theme={defaultTheme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
+  </React.StrictMode>
 );
---
+++ src/styles/theme/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  COLORS: {
+    WHITE: "#FFFFFF",
+    BLACK: "#000000",
+
+    GRAY_100: "#E5E5E5",
+    GRAY_200: "#F4F4F4",
+
+    GREEN_500: "#1DB954",
+  },
+  FONT_FAMILY: {
+    primary: "Montserrat, sans-serif",
+  },
+  FONT_WEIGHT: {
+    REGULAR_400: 400,
+    BOLD_600: 600,
+    BOLD_700: 700,
+    BOLD_800: 800,
+  },
+} as const;
